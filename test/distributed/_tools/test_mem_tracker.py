975916805a6b70414a2ec6304fc749b4aea167d7
libohao <libo.hao@intel.com>
2025-07-30 17:01:42 +0800

    we will port distributed tests to Intel GPU. We could enable Intel GPU with following methods and try the best to keep the original code styles:
    1. use "torch.accelerator.current_accelerator()" to determine the accelerator backend
    2. enabled XPU for some test path
    3. skip some test cases which Intel GPU does not support
    
    Resovle conflicts:

--- test/distributed/_tools/test_mem_tracker.py
+++ test/distributed/_tools/test_mem_tracker.py
@@ -33,6 +33,7 @@ class TestMemTracker(TestCase):
     @unittest.skipIf(
         not TEST_CUDA and not TEST_XPU, "Neither CUDA or XPU is not available"
     )
+
     def test_accelerator_tracker_equivalence(
         self,
     ):
